262149c5c5092a1a49aea3ffd85ff2dc9b982fe1
brigo <brigovaczbalazs@hotmail.com>
2026-09-20 09:27:48 +0200

    61. Excluding Response Properties

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/updarte-user.dto';
@@ -13,6 +13,7 @@ export class UsersController {
 		this.usersService.create(body.email, body.password);
 	}
 
+	@UseInterceptors(ClassSerializerInterceptor)
 	@Get('/:id')
 	async findUser(@Param('id') id: string) {
 		const user = await this.usersService.findOne(parseInt(id));
