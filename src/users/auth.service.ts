00c1e28c621ad00141dc70bfd3b2aac12decbc88
brigo <brigovaczbalazs@hotmail.com>
2026-09-21 17:24:06 +0200

    74. Creating a User

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { randomBytes, scrypt as _script } from "crypto";
 import { promisify } from "util";
@@ -10,7 +10,10 @@ export class AuthService {
 
 	async signup(email: string, password: string) {
 		//See if email is in use
-		const user = this.usersService.find(email);
+		const users = await this.usersService.find(email);
+		if (users.length) {
+			throw new BadRequestException("Email in use");
+		}
 		//Hash the users password
 		//Generate a salt
 		const salt = randomBytes(8).toString("hex");
@@ -19,8 +22,9 @@ export class AuthService {
 		//Join the hashed result and the salt together
 		const result = salt + "." + hash.toString("hex");
 		//Create a new user and save it
-
+		const user = this.usersService.create(email, result);
 		//Return the user
+		return user;
 	}
 
 	signin() {
