058a55e656a561fe71b3862f2e4c780d78df62a9
brigo <brigovaczbalazs@hotmail.com>
2026-09-21 14:34:25 +0200

    71. Implementing Signup Functionality

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -4,4 +4,17 @@ import { UsersService } from "./users.service";
 @Injectable()
 export class AuthService {
 	constructor(private usersService: UsersService) {}
+
+	signup(email: string, password: string) {
+		//See if email is in use
+		const user = this.usersService.find(email);
+		//Hash the users password
+
+		//Create a new user and save it
+
+		//Return the user
+	}
+
+	signin() {
+	}
 }
