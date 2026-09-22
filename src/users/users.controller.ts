09defccfe19932c81d8d3000372da43255a8280e
brigo <brigovaczbalazs@hotmail.com>
2026-09-22 08:27:43 +0200

    78. Signing in a User

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query} from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, Session} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/updarte-user.dto';
@@ -16,13 +16,17 @@ export class UsersController {
 	) {}
 
 	@Post('signup')
-	createUser(@Body() body: CreateUserDto) {
-		this.authService.signup(body.email, body.password);
+	 async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+		const user = await this.authService.signup(body.email, body.password);
+		session.userId = user.id;
+		return user;
 	}
 
 	@Post('signin')
-	async signin(@Body() body: CreateUserDto) {
-		return await this.authService.signin(body.email, body.password);
+	async signin(@Body() body: CreateUserDto, @Session() session: any) {
+		const user = await this.authService.signin(body.email, body.password);
+		session.userId = user.id;
+		return user;
 	}
 
 	@Get('/:id')
