eaca8515496d6c85bd08cb274905794cc7e0a6c9
brigo <brigovaczbalazs@hotmail.com>
2026-09-22 10:12:04 +0200

    82. Custom Param Decorators

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,6 +5,7 @@ import { UpdateUserDto } from './dtos/updarte-user.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
 
 @Serialize(UserDto)
 @Controller('auth')
@@ -15,9 +16,14 @@ export class UsersController {
 		private authService: AuthService
 	) {}
 
+	// @Get('/whoami')
+	// whoAmI(@Session() session: any) {
+	// 	return this.usersService.findOne(session.userId);
+	// }
+
 	@Get('/whoami')
-	whoAmI(@Session() session: any) {
-		return this.usersService.findOne(session.userId);
+	whoAmI(@CurrentUser() user: string) {
+		return user;
 	}
 
 	@Post('signout')
