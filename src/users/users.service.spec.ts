d8b5d5baa3177ada0055de4f06bbcdb571a316ed
brigo <brigovaczbalazs@hotmail.com>
2026-09-23 17:21:08 +0200

    99. Testing the Signin Flow

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -2,7 +2,7 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './users.entity';
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -48,5 +48,12 @@ describe('AuthService', () => {
       BadRequestException,
     );
   });
+
+  it('throws if signin is called with an unused email', async () => {
+    await expect(
+      service.signin('asdflkj@asdlfkj.com', 'passdflkj'),
+    ).rejects.toThrow(NotFoundException);
+  });
+
 });
 
